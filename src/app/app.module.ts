426a672dbb1e94c72f33c0c70676f899d7ba0919
gour9808 <gouravsharma614@gmail.com>
2018-08-29 07:59:42 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatTableModule, NoopAnimationsModule, BrowserAnimationsModule,
+    MatToolbarModule, MatIconModule, MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
